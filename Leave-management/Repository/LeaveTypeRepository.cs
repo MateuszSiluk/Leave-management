37c2dad5c2a4a5f622da2419c66bfed6014573a5
Mateusz Siluk <mati169@hotmail.com>
2020-11-22 19:02:43 +0000

    Setup Repository and Dependency Injection

---
+++ Leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,52 @@
+﻿using Leave_management.Contracts;
+using Leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_management.Repository
+{
+    public class LeaveTypeRepository : Contracts.ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveType FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
