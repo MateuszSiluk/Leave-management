f64d8534073336d8594785e0a6b25689b3f0bbbf
Mateusz Siluk <mati169@hotmail.com>
2020-11-29 19:15:27 +0000

    Add Default Roles and Admin User

--- Leave-management/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Leave-management/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -87,11 +87,16 @@ namespace Leave_management.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new Employee { UserName = Input.Email, Email = Input.Email, 
-                                          FirstName = Input.FirstName, LastName=Input.LastName };
+                var user = new Employee { UserName = Input.Email,
+                                          Email = Input.Email, 
+                                          FirstName = Input.FirstName, 
+                                          LastName=Input.LastName };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, "Employee").Wait();
+
                     _logger.LogInformation("User created a new account with password.");
 
                     
