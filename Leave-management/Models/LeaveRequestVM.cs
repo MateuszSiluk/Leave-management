ccb806a61943e27fec59990d6219fbc02d7c63a9
Mateusz Siluk <mateusz.siluk@gmail.com>
2020-12-11 20:21:18 +0000

    Setup Administrator Index View

--- Leave-management/Models/LeaveRequestVM.cs
+++ Leave-management/Models/LeaveRequestVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,9 +27,13 @@ namespace Leave_management.Models
 
     public class AdminLeaveRequestViewVM
     {
-        public int TotalRequests { get; set; }
+        [Display(Name = "Total Number Of Requests")]
+        public int TotalRequests { get; set; } 
+        [Display(Name ="Approved Requests")]
         public int ApprovedRequests { get; set; }
+        [Display(Name ="Pending Requests")]
         public int PendingRequests { get; set; }
+        [Display(Name = "Rejected Requests")]
         public int RejectedRequests { get; set; }
         public List<LeaveRequestVM> LeaveRequests { get; set; }
     }
